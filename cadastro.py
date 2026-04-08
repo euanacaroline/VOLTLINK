1aca931b3d51268b0d6191ba7334a012a8b99cc2
euanacaroline <acarolisbn@gmail.com>
2026-04-08 17:58:32 -0300

    atualização cadastro

--- cadastro.py
+++ cadastro.py
@@ -3,29 +3,62 @@ import os
 #variáveis
 email_cadastrado = ""
 senha_cadastrada = ""
+número_contato = ""
+
+# Lista de usuários já cadastrados (simulando um banco de dados)
+usuarios_existentes = ["teste@gmail.com", "jose@ufrpe.br"]
 
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def tela_cadastro(): 
-    global email_cadastrado, senha_cadastrada
+    global email_cadastrado, senha_cadastrada, número_contato
     limpar_tela()
     print("   CADASTRO VOLTLINK   ")
-    email_cadastrado = input("Digite seu email: ")
+
     while True:
-        senha = input("Crie uma senha: ")
-        confirmacao = input("Confirme sua senha: ")
-        
+        email = input("Digite seu email:  ").strip().lower()
+        if not email: 
+             print(" Erro: O campo e-mail não pode estar vazio.")
+        elif "@" not in email or "." not in email or " " in email:
+             print(" Erro: E-mail inválido! Certifique-se de que não há espaços e que possui '@' e '.'")
+        elif "ufrpe" not in email and "gmail" not in email:
+             print( " Erro: Aceitamos apenas email da UFRPE ou Gmail.")
+        elif not (email.endswith(".com") or email.endswith(".br")):
+             print(" Erro: O e-mail deve terminar em .com ou .br")
+        elif email in usuarios_existentes:
+             print(" Erro: Email já cadastrado!")
+        else: 
+             email_cadastrado = email
+             print(f" Email {email_cadastrado} validado!")
+             break 
+
+
+    while True:
+        tel = input("Digite seu telefone (11 dígitos): ").strip()
+        if not tel.isdigit():
+            print(" Erro: Digite apenas números.")
+        elif len(tel) != 11:
+            print(" Erro: O telefone precisa de exatamente 11 dígitos.")
+        else:
+            numero_contato = tel
+            break 
+    
+    while True:
+        senha = input("Crie uma senha: ").strip()
+        confirmacao = input("Confirme sua senha: ").strip()
         if senha == confirmacao:
             senha_cadastrada = senha
-            print("\n✅ Senhas conferem! Cadastro realizado.")
-            break  # Sai do loop e finaliza a função
+            print("Senha validada e cadastrada!")
+            break
         else:
-            print("\n❌ As senhas não coincidem! Tente novamente.")
-            # O loop volta para o início do 'while True'
-            
+             print(" Erro: As senhas não coincidem. Tente novamente.")
+
+    print("Cadastro realizado com sucesso!")
+    print(f"Usuário: {email_cadastrado}")
     input("\nPressione Enter para voltar ao menu...")
    
+
 #menu usuário
 def menu_entrada():
     while True:
@@ -45,13 +78,14 @@ def menu_entrada():
             print("\nObrigado por usar o VoltLink! Saindo...")
             break 
         else:
-            print("\n⚠️ Opção inválida! Tente novamente.")
+            print("\n Opção inválida! Tente novamente.")
             input("Aperte Enter para continuar...")
 
-# iniciar o programa chamando o menu
+          
+# --- O FINAL DO ARQUIVO É AQUI ---
 if __name__ == "__main__":
     menu_entrada()
-
+            
 
 
 
